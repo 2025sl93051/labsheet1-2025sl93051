ae1a40c7d85349f6120fc55d2e860d0addabbb0f
Bandravally Naveen Kumar <2025sl93051@wilp.bits-pilani.ac.in>
2026-04-04 16:49:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,12 +2,9 @@ def add(a,b):
      return a+b
 def multiply(a,b):
      return a*b
-<<<<<<< HEAD
 def subtract(a,b):
     return a-b
-=======
 def divide(a,b):
     if b==0:
         return None 
     return a/b
->>>>>>> 631fd0f (Add divide function)
